ce92341895ce45fc8dc0d47060d58a2b842ebcea
codenameharsh <harshitha167@gmail.com>
2025-01-21 16:31:06 -0800

    css styling

---
+++ meme_generator/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  const [meme, setMeme] = useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: 'https://i.imgflip.com/32ij86.jpg'
+})
+
+function handleChange(event) {
+    const {value, name} = event.currentTarget
+    setMeme(prevMeme => ({
+        ...prevMeme,
+        [name]: value
+    
+    }))
+}
+
+
+  return (
+    <>
+
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.imageUrl} alt='meme-template'/>
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    </>
+  )
+}
+
+export default App
